c893628c923a491c96279c76fd2e9cfa81de2cb0
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-08-07 00:34:00 +0200

    feat: create a file for const CITIES

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,21 +1,7 @@
 import React from "react";
+import CITIES from "../consts/CITIES";
 
 function Select() {
-  const CITIES = [
-    {
-      city: "Munich",
-      lstitude: 48.1374,
-      longitude: 11.5755,
-      id: 1,
-    },
-    {
-      city: "Tel-Aviv",
-      latitude: 32.0809,
-      longitude: 34.7806,
-      id: 2,
-    },
-  ];
-
   const optionsCities = CITIES.map((city) => (
     <option key={city.id} value={city.id}>
       {city.city}
---
+++ src/consts/CITIES.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CITIES = [
+  {
+    city: "Munich",
+    lstitude: 48.1374,
+    longitude: 11.5755,
+    id: 1,
+  },
+  {
+    city: "Tel-Aviv",
+    latitude: 32.0809,
+    longitude: 34.7806,
+    id: 2,
+  },
+];
+
+export default CITIES
