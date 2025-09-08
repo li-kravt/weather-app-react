60aa39ca43bd8da603d773725c51dcce18fbbc48
li-kravt <215831814+li-kravt@users.noreply.github.com>
2025-09-08 15:02:45 +0200

    solution: simple checkbox task with useState (text)

--- src/components/tasks/UseStateNumbers1.jsx
+++ src/components/tasks/UseStateNumbers1.jsx
@@ -4,7 +4,7 @@ export default function CounterButtonPlusMinus() {
   const [num, setNum] = useState(0)
 
   return (
-    <div className="flex flex-col w-auto border-2">
+    <div className="flex flex-col items-start p-4 w-auto border-2">
       <p className="font-bold">CounterButtonPlusMinus</p>
     <p>{num}</p>
     <button onClick={() => setNum(num + 1)}>+</button>
